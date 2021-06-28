942a10bab723d310790eadeb0c01728bd6f7cc31
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2021-06-28 03:10:20 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -14,17 +14,28 @@ class Customer
   end
   
   def new_meal (waiter, total, tip=0)
+<<<<<<< HEAD
     #initializes a meal using the current Customer instance, a provided Waiterinstance and a total and tip
     Meal.new(waiter, self, total, tip)
   end
   
   def meals #returns an Array of Meal instances associated with this customer
+=======
+    Meal.new(waiter, self, total, tip)
+  end
+  
+  def meals
+>>>>>>> d703c4cbe87c12bd944ccde395353ec86d943450
     Meal.all.select do |meal|
       meal.customer == self 
     end
   end
   
+<<<<<<< HEAD
   def waiters #returns an Array of Waiter instances associated with this customer's meal
+=======
+  def waiters 
+>>>>>>> d703c4cbe87c12bd944ccde395353ec86d943450
     meals.map do |meal|
       meal.waiter 
     end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -13,19 +13,33 @@ class Waiter
     @@all 
   end
   
+<<<<<<< HEAD
   def new_meal(customer, total, tip=0) #initializes a meal using the current Waiter instance, a provided Customerinstance and a total and tip
     Meal.new(self, customer, total, tip)
   end
   
   def meals # returns an Array of Meal instances associated with this waiter
+=======
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip)
+  end
+  
+  def meals
+>>>>>>> d703c4cbe87c12bd944ccde395353ec86d943450
     Meal.all.select do |meal|
       meal.waiter == self 
     end
   end
   
+<<<<<<< HEAD
   def best_tipper #returns the Customer instance associated with the meal that received the largest tip
     best_tipped_meal = meals.max do |meal_a, meal_b|
       meal_a.tip <=> meal_b.tip 
+=======
+  def best_tipper 
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+      meal_a.top <=> meal_b.tip 
+>>>>>>> d703c4cbe87c12bd944ccde395353ec86d943450
     end 
     
     best_tipped_meal.customer
